1f967c54a47f8ffc4363c0952967485871b0f527
81903 <81903@192.168.1.8>
2026-03-14 10:35:55 +0900

    自己紹介文の作成

---
+++ HelloWorld_yuyakako/src/sample/Selflntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Selflntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "加古 勇也";
+		// 年齢
+		int age = 27;
+		// 趣味
+		String hobby = "ゲーム";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
